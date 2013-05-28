928947fe0b2d4de6f73b4809405b01038335f2d5
Emmanuel Addo-Yirenkyi <emmanuel.addo.yirenkyi@gmail.com>
2013-05-28 14:22:10 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicConsult::Application.config.secret_token = '2487596147b259ecb3e558be751d6350226ed90a40de685cf30d9dcddaaae1c675559fddbb7627c3534fc358639cf564f76eb940ce953d61a0c2480433fd4877'
